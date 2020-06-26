ce93cdf5c166c3563987a02778d86a7cd77e36f7
Giuliana Evangelina Olmos <giulianaolmos@gmail.com>
2020-06-26 15:56:06 -0300

    initial commit

---
+++ src/pages/Chat/Chat.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Chat.css'; 
+
+const Chat = () => {
+    return(
+            <div>
+                Soy un Chat
+            </div>
+    )
+}
+
+export default Chat;
